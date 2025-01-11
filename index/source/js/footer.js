2bbf3fb9f438eb0b55d7dbe4ffa47156ca278543
darkHaloCn <77whitemoon@gmail.com>
2025-01-11 23:44:04 +0800

    chonggou
    copy tutu index

---
+++ index/source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2021-2022 TuHome</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
